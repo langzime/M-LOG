b7f8ba19693fc3a891acf76f1b9674ffe99afae2
高尤波 <gaoyoubo@foxmail.com>
2013-03-18 17:10:44 +0800

    User manytoone

--- mlog-service/src/main/java/org/mspring/mlog/entity/Attachment.java
+++ mlog-service/src/main/java/org/mspring/mlog/entity/Attachment.java
@@ -12,6 +12,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -92,7 +93,7 @@ public class Attachment implements Serializable {
     }
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false, targetEntity = User.class)
-    @Column(name = "user")
+    @JoinColumn(name = "user")
     public User getUser() {
         return user;
     }
