47a24a3ebdaee36d70eab95a754a94551d639b66
高尤波 <gaoyoubo@foxmail.com>
2013-04-02 14:49:00 +0800

    退出后跳转到首页

--- mlog-web/src/main/java/org/mspring/mlog/web/module/admin/LoginWidget.java
+++ mlog-web/src/main/java/org/mspring/mlog/web/module/admin/LoginWidget.java
@@ -9,12 +9,10 @@ import javax.servlet.http.HttpSession;
 
 import org.mspring.mlog.common.Keys;
 import org.mspring.mlog.entity.security.User;
-import org.mspring.mlog.service.security.UserService;
 import org.mspring.mlog.support.log.Log;
 import org.mspring.mlog.web.freemarker.widget.stereotype.Widget;
 import org.mspring.mlog.web.security.SecurityUtils;
 import org.mspring.platform.utils.CookieUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,15 +28,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/admin")
 public class LoginWidget extends AbstractAdminWidget {
 
-    private static final String MESSAGE = "message";
-
-    private UserService userService;
-
-    @Autowired
-    public void setUserService(UserService userService) {
-        this.userService = userService;
-    }
-
     @RequestMapping(value = { "/login" }, method = { RequestMethod.GET })
     @Log
     public String login(@ModelAttribute User user, HttpServletRequest request) {
@@ -53,6 +42,6 @@ public class LoginWidget extends AbstractAdminWidget {
     public String logout(@ModelAttribute User user, HttpServletRequest request, HttpServletResponse response, HttpSession session, Model model) {
         session.invalidate();
         CookieUtils.setCookie(response, Keys.IS_REMEMBER_USER_COOKIE, "false", 365);
-        return "/admin/login";
+        return "redirect:/";
     }
 }
