61890b1495913a1f05d96466950147704b8db2b4
高尤波 <gaoyoubo@foxmail.com>
2013-04-02 11:26:04 +0800

    解决代码冲突。

--- mlog-web/src/main/java/org/mspring/mlog/web/module/user/User_CatalogWidget.java
+++ mlog-web/src/main/java/org/mspring/mlog/web/module/user/User_CatalogWidget.java
@@ -25,8 +25,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * SNS类别管理
+ * 
  * @author huhongyu
- *
+ * 
  */
 @Widget
 @RequestMapping("user/catalog")
@@ -41,6 +42,7 @@ public class User_CatalogWidget extends AbstractUserWidget {
 
     /**
      * 展现分类列表
+     * 
      * @param catalogPage
      * @param catalog
      * @param queryParams
@@ -52,31 +54,32 @@ public class User_CatalogWidget extends AbstractUserWidget {
     @RequestMapping("/list")
     @Log
     public String listCatalog(@ModelAttribute Page<Catalog> catalogPage, @ModelAttribute Catalog catalog, @QueryParam Map queryParams, HttpServletRequest request, HttpServletResponse response, Model model) {
-    	
-    	User loginUser = SecurityUtils.getCurrentUser();
-    	if (catalogPage == null) {
+
+        User loginUser = SecurityUtils.getCurrentUser();
+        if (catalogPage == null) {
             catalogPage = new Page<Catalog>();
             catalogPage.setPageSize(12);
         }
-    	
-    	 Sort sort = new Sort();
-         sort.setField("order");
-         sort.setOrder("desc");
-         catalogPage.setSort(sort);
-         
-    	if(loginUser!=null){
-    		queryParams.put("user.id", loginUser.getId());
-    	}
-    	
+
+        Sort sort = new Sort();
+        sort.setField("order");
+        sort.setOrder("desc");
+        catalogPage.setSort(sort);
+
+        if (loginUser != null) {
+            queryParams.put("user.id", loginUser.getId());
+        }
+
         catalogPage = catalogService.findCatalog(catalogPage, new CatalogQueryCriterion(queryParams));
 
         model.addAttribute("catalogs", CatalogUtils.getTreeList(catalogService.findAllCatalog()));
         model.addAttribute("catalogPage", catalogPage);
         return view("catalog/list");
     }
-    
+
     /**
      * 删除分类-包括批量删除
+     * 
      * @param catalogPage
      * @param catalog
      * @param queryParams
@@ -96,9 +99,10 @@ public class User_CatalogWidget extends AbstractUserWidget {
         }
         return listCatalog(catalogPage, catalog, queryParams, request, response, model);
     }
-    
+
     /**
      * 创建新的分类
+     * 
      * @param catalog
      * @param request
      * @param response
@@ -107,16 +111,17 @@ public class User_CatalogWidget extends AbstractUserWidget {
      */
     @RequestMapping("/create")
     @Log
-    public String create(@ModelAttribute Catalog catalog , HttpServletRequest request,HttpServletResponse response ,Model model){
-    	if(catalog.getCreateTime()==null){
-    		catalog.setCreateTime(new Date());
-    	}
-    	catalogService.createCatalog(catalog);
-    	return "redirect:"+view("catalog/list");
+    public String create(@ModelAttribute Catalog catalog, HttpServletRequest request, HttpServletResponse response, Model model) {
+        if (catalog.getCreateTime() == null) {
+            catalog.setCreateTime(new Date());
+        }
+        catalogService.createCatalog(catalog);
+        return "redirect:" + view("catalog/list");
     }
-    
+
     /**
      * 修改分类
+     * 
      * @param catalog
      * @param request
      * @param response
@@ -125,157 +130,14 @@ public class User_CatalogWidget extends AbstractUserWidget {
      */
     @RequestMapping("modify")
     @Log
-    public String modify(@ModelAttribute Catalog catalog , HttpServletRequest request,HttpServletResponse response ,Model model){
-    	if(catalog.getId()!=null&&catalog.getId()!=0){
-    		catalog.setCreateTime(catalogService.getCatalogById(catalog.getId()).getCreateTime());
-    		catalog.setUser(SecurityUtils.getCurrentUser());
-    		catalog.setModifyTime(new Date());
-    		catalogService.updateCatalog(catalog);
-    	}
-    	return "redirect:"+view("catalog/list");
+    public String modify(@ModelAttribute Catalog catalog, HttpServletRequest request, HttpServletResponse response, Model model) {
+        if (catalog.getId() != null && catalog.getId() != 0) {
+            catalog.setCreateTime(catalogService.getCatalogById(catalog.getId()).getCreateTime());
+            catalog.setUser(SecurityUtils.getCurrentUser());
+            catalog.setModifyTime(new Date());
+            catalogService.updateCatalog(catalog);
+        }
+        return "redirect:" + view("catalog/list");
     }
-    
+
 }
-=======
-package org.mspring.mlog.web.module.user;
-
-import java.util.Date;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.mspring.mlog.entity.Catalog;
-import org.mspring.mlog.entity.security.User;
-import org.mspring.mlog.service.CatalogService;
-import org.mspring.mlog.support.log.Log;
-import org.mspring.mlog.support.resolver.QueryParam;
-import org.mspring.mlog.utils.CatalogUtils;
-import org.mspring.mlog.web.freemarker.widget.stereotype.Widget;
-import org.mspring.mlog.web.query.CatalogQueryCriterion;
-import org.mspring.mlog.web.security.SecurityUtils;
-import org.mspring.platform.persistence.support.Page;
-import org.mspring.platform.persistence.support.Sort;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-/**
- * SNS类别管理
- * @author huhongyu
- *
- */
-@Widget
-@RequestMapping("user/catalog")
-public class User_CatalogWidget extends AbstractUserWidget {
-
-    private CatalogService catalogService;
-
-    @Autowired
-    public void setCatalogService(CatalogService catalogService) {
-        this.catalogService = catalogService;
-    }
-
-    /**
-     * 展现分类列表
-     * @param catalogPage
-     * @param catalog
-     * @param queryParams
-     * @param request
-     * @param response
-     * @param model
-     * @return
-     */
-    @RequestMapping("/list")
-    @Log
-    public String listCatalog(@ModelAttribute Page<Catalog> catalogPage, @ModelAttribute Catalog catalog, @QueryParam Map queryParams, HttpServletRequest request, HttpServletResponse response, Model model) {
-    	
-    	User loginUser = SecurityUtils.getCurrentUser();
-    	if (catalogPage == null) {
-            catalogPage = new Page<Catalog>();
-            catalogPage.setPageSize(12);
-        }
-    	
-    	 Sort sort = new Sort();
-         sort.setField("order");
-         sort.setOrder("desc");
-         catalogPage.setSort(sort);
-         
-    	if(loginUser!=null){
-    		queryParams.put("user.id", loginUser.getId());
-    	}
-    	
-        catalogPage = catalogService.findCatalog(catalogPage, new CatalogQueryCriterion(queryParams));
-
-        model.addAttribute("catalogs", CatalogUtils.getTreeList(catalogService.findAllCatalog()));
-        model.addAttribute("catalogPage", catalogPage);
-        return view("catalog/list");
-    }
-    
-    /**
-     * 删除分类-包括批量删除
-     * @param catalogPage
-     * @param catalog
-     * @param queryParams
-     * @param ids
-     * @param deleteIds
-     * @param orders
-     * @param request
-     * @param response
-     * @param model
-     * @return
-     */
-    @RequestMapping("ctrl")
-    @Log
-    public String ctrl(@ModelAttribute Page<Catalog> catalogPage, @ModelAttribute Catalog catalog, @QueryParam Map queryParams, @RequestParam(required = false) Long[] ids, @RequestParam(required = false) Long[] deleteIds, @RequestParam(required = false) Long[] orders, HttpServletRequest request, HttpServletResponse response, Model model) {
-        if (deleteIds != null && deleteIds.length > 0) {
-            catalogService.deleteCatalog(deleteIds);
-        }
-        if (orders != null && orders.length > 0) {
-            catalogService.setCatalogOrders(ids, orders);
-        }
-        return listCatalog(catalogPage, catalog, queryParams, request, response, model);
-    }
-    
-    /**
-     * 创建新的分类
-     * @param catalog
-     * @param request
-     * @param response
-     * @param model
-     * @return
-     */
-    @RequestMapping("/create")
-    @Log
-    public String create(@ModelAttribute Catalog catalog , HttpServletRequest request,HttpServletResponse response ,Model model){
-    	if(catalog.getCreateTime()==null){
-    		catalog.setCreateTime(new Date());
-    	}
-    	catalogService.createCatalog(catalog);
-    	return "redirect:"+view("catalog/list");
-    }
-    
-    /**
-     * 修改分类
-     * @param catalog
-     * @param request
-     * @param response
-     * @param model
-     * @return
-     */
-    @RequestMapping("modify")
-    @Log
-    public String modify(@ModelAttribute Catalog catalog , HttpServletRequest request,HttpServletResponse response ,Model model){
-    	if(catalog.getId()!=null&&catalog.getId()!=0){
-    		catalog.setCreateTime(catalogService.getCatalogById(catalog.getId()).getCreateTime());
-    		catalog.setUser(SecurityUtils.getCurrentUser());
-    		catalog.setModifyTime(new Date());
-    		catalogService.updateCatalog(catalog);
-    	}
-    	return "redirect:"+view("catalog/list");
-    }
-    
-}
->>>>>>> branch 'master' of https://github.com/M-LOG/M-LOG.git
--- mlog-web/src/main/java/org/mspring/mlog/web/module/user/User_CommentWidget.java
+++ mlog-web/src/main/java/org/mspring/mlog/web/module/user/User_CommentWidget.java
@@ -25,27 +25,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 @SuppressWarnings({ "unchecked", "rawtypes" })
 @Widget
 @RequestMapping("user/comment")
-public class User_CommentWidget extends AbstractUserWidget{
+public class User_CommentWidget extends AbstractUserWidget {
 
-	private CommentService commentService;
-	
-	@Autowired
-	public void setCommentService(CommentService commentService) {
-		this.commentService = commentService;
-	}
-	
-	/**
-	 * 获取评论列表
-	 * @param commentPage
-	 * @param comment
-	 * @param queryParams
-	 * @param request
-	 * @param response
-	 * @param model
-	 * @return
-	 */
-	@Log
-	@RequestMapping("/list")
+    private CommentService commentService;
+
+    @Autowired
+    public void setCommentService(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    /**
+     * 获取评论列表
+     * 
+     * @param commentPage
+     * @param comment
+     * @param queryParams
+     * @param request
+     * @param response
+     * @param model
+     * @return
+     */
+    @Log
+    @RequestMapping("/list")
     public String listComment(@ModelAttribute Page<Comment> commentPage, @ModelAttribute Comment comment, @QueryParam Map queryParams, HttpServletRequest request, HttpServletResponse response, Model model) {
         if (comment == null) {
             comment = new Comment();
@@ -56,132 +57,76 @@ public class User_CommentWidget extends AbstractUserWidget{
         commentPage.setSort(new Sort("id", Sort.DESC));
         commentPage.pageSize(13);
         User loginUser = SecurityUtils.getCurrentUser();
-        
-        if(loginUser!=null){
-        	queryParams.put("user.id", loginUser.getId());
+
+        if (loginUser != null) {
+            queryParams.put("user.id", loginUser.getId());
         }
-        
+
         QueryCriterion queryCriterion = new CommentQueryCriterion(queryParams);
         commentPage = commentService.findComment(commentPage, queryCriterion);
         model.addAttribute("commentPage", commentPage);
         return view("comment/list");
     }
-	
-	/**
-	 * 删除评论
-	 * @param deleteIds
-	 * @param ids
-	 * @param commentPage
-	 * @param comment
-	 * @param queryParams
-	 * @param request
-	 * @param response
-	 * @param model
-	 * @return
-	 */
-	@Log
-	@RequestMapping("/delete")
-	public String deleteComments(@RequestParam(required = false) Long[] deleteIds,@RequestParam(required = false) Long[] ids, @ModelAttribute Page<Comment> commentPage, @ModelAttribute Comment comment, @QueryParam Map queryParams, HttpServletRequest request, HttpServletResponse response, Model model){
-		if(deleteIds!=null && deleteIds.length>0){
-			commentService.deleteComment(deleteIds);
-		}
-		return listComment(commentPage, comment, queryParams, request, response, model);
-	}
-	
-	/**
-	 * 显示该评论的详细信息
-	 * @param id
-	 * @param commentPage
-	 * @param comment
-	 * @param queryParams
-	 * @param request
-	 * @param response
-	 * @param model
-	 * @return
-	 */
-	@Log
-	@RequestMapping("/auditView")
-	public String showCommentView(@RequestParam(required = false) Long id, @ModelAttribute Page<Comment> commentPage, @ModelAttribute Comment comment, @QueryParam Map queryParams, HttpServletRequest request, HttpServletResponse response, Model model){
-		if(comment == null){
-			comment = commentService.getCommentById(id);
-		}
-		model.addAttribute("comment", comment);
-		return view("comment/view");
-	}
-	
-	/**
-	 * 审核评论
-	 * @param commentPage
-	 * @param comment
-	 * @param queryParams
-	 * @param request
-	 * @param response
-	 * @param model
-	 * @return 评论列表
-	 */
-	@Log
-	@RequestMapping("/audit")
-	public String auditComment( @ModelAttribute Page<Comment> commentPage, @ModelAttribute Comment comment, @QueryParam Map queryParams, HttpServletRequest request, HttpServletResponse response, Model model){
-		
-		return listComment(commentPage, comment, queryParams, request, response, model);
-	}
-}
-=======
-package org.mspring.mlog.web.module.user;
 
-import java.util.Map;
+    /**
+     * 删除评论
+     * 
+     * @param deleteIds
+     * @param ids
+     * @param commentPage
+     * @param comment
+     * @param queryParams
+     * @param request
+     * @param response
+     * @param model
+     * @return
+     */
+    @Log
+    @RequestMapping("/delete")
+    public String deleteComments(@RequestParam(required = false) Long[] deleteIds, @RequestParam(required = false) Long[] ids, @ModelAttribute Page<Comment> commentPage, @ModelAttribute Comment comment, @QueryParam Map queryParams, HttpServletRequest request, HttpServletResponse response, Model model) {
+        if (deleteIds != null && deleteIds.length > 0) {
+            commentService.deleteComment(deleteIds);
+        }
+        return listComment(commentPage, comment, queryParams, request, response, model);
+    }
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.mspring.mlog.entity.Comment;
-import org.mspring.mlog.entity.security.User;
-import org.mspring.mlog.service.CommentService;
-import org.mspring.mlog.support.log.Log;
-import org.mspring.mlog.support.resolver.QueryParam;
-import org.mspring.mlog.web.freemarker.widget.stereotype.Widget;
-import org.mspring.mlog.web.query.CommentQueryCriterion;
-import org.mspring.mlog.web.security.SecurityUtils;
-import org.mspring.platform.persistence.query.QueryCriterion;
-import org.mspring.platform.persistence.support.Page;
-import org.mspring.platform.persistence.support.Sort;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Widget
-@RequestMapping("user/comment")
-public class User_CommentWidget extends AbstractUserWidget{
-
-	private CommentService commentService;
-	
-	@Autowired
-	public void setCommentService(CommentService commentService) {
-		this.commentService = commentService;
-	}
-	
-	@Log
-	@RequestMapping("/list")
-    public String listComment(@ModelAttribute Page<Comment> commentPage, @ModelAttribute Comment comment, @QueryParam Map queryParams, HttpServletRequest request, HttpServletResponse response, Model model) {
+    /**
+     * 显示该评论的详细信息
+     * 
+     * @param id
+     * @param commentPage
+     * @param comment
+     * @param queryParams
+     * @param request
+     * @param response
+     * @param model
+     * @return
+     */
+    @Log
+    @RequestMapping("/auditView")
+    public String showCommentView(@RequestParam(required = false) Long id, @ModelAttribute Page<Comment> commentPage, @ModelAttribute Comment comment, @QueryParam Map queryParams, HttpServletRequest request, HttpServletResponse response, Model model) {
         if (comment == null) {
-            comment = new Comment();
+            comment = commentService.getCommentById(id);
         }
-        if (commentPage == null) {
-            commentPage = new Page<Comment>();
-        }
-        commentPage.setSort(new Sort("id", Sort.DESC));
-        
-        User loginUser = SecurityUtils.getCurrentUser();
-        
-        if(loginUser!=null){
-        	queryParams.put("user.id", loginUser.getId());
-        }
-        
-        QueryCriterion queryCriterion = new CommentQueryCriterion(queryParams);
-        commentPage = commentService.findComment(commentPage, queryCriterion);
-        model.addAttribute("commentPage", commentPage);
-        return view("comment/list");
+        model.addAttribute("comment", comment);
+        return view("comment/view");
+    }
+
+    /**
+     * 审核评论
+     * 
+     * @param commentPage
+     * @param comment
+     * @param queryParams
+     * @param request
+     * @param response
+     * @param model
+     * @return 评论列表
+     */
+    @Log
+    @RequestMapping("/audit")
+    public String auditComment(@ModelAttribute Page<Comment> commentPage, @ModelAttribute Comment comment, @QueryParam Map queryParams, HttpServletRequest request, HttpServletResponse response, Model model) {
+
+        return listComment(commentPage, comment, queryParams, request, response, model);
     }
 }
->>>>>>> branch 'master' of https://github.com/M-LOG/M-LOG.git
